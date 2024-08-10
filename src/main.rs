38e8ce15de2b44e515259aba2c1c4ec9c39197bf
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-08-09 21:36:44 -0400

    Include assets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::fs;
+use std::path::Path;
 use egui::Vec2;
+use include_assets::{include_dir, NamedArchive};
 use open_lights_manager::gui;
 
 fn main() -> eframe::Result<()> {
-
+    extract_assets();
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_close_button(true)
@@ -32,4 +35,17 @@ fn main() -> eframe::Result<()> {
             Ok(Box::new(gui::OpenLightsManager::new(&cc.egui_ctx)))
         }),
     )
+}
+
+fn extract_assets() {
+    let assets = NamedArchive::load(include_dir!("assets"));
+    let assets_path = Path::new("assets");
+    if !assets_path.exists() {
+        fs::create_dir(assets_path).unwrap();
+    }
+
+    for asset in assets.assets() {
+        let asset_path = assets_path.join(asset.0);
+        fs::write(asset_path, asset.1).unwrap();
+    }
 }
